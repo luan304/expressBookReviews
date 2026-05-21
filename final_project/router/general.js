e4b3e491f10e0514ed61bf213a10b2e446c9a711
luan304 <trieuluan2005@gmail.com>
2026-05-21 15:39:12 +0700

    Implement book retrieval functions using axios
    Added asynchronous and promise-based functions to fetch books by various criteria.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,36 @@
-const express = require('express');
-let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
-const public_users = express.Router();
+const axios = require('axios');
+const localURL = "http://localhost:5000";
 
+// Lấy tất cả sách (Async/Await)
+const getAllBooksAsync = async () => {
+  try {
+    const response = await axios.get(localURL + "/");
+    console.log("Task 11 - All Books:", response.data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+// Lấy sách theo ISBN (Promise Callback)
+const getBookByIsbnPromise = (isbn) => {
+  axios.get(localURL + "/isbn/" + isbn)
+    .then(response => console.log(`Task 11 - Book ISBN ${isbn}:`, response.data))
+    .catch(error => console.error("Error:", error));
+};
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+// Lấy sách theo Tác giả (Async/Await)
+const getBookByAuthorAsync = async (author) => {
+  try {
+    const response = await axios.get(localURL + "/author/" + author);
+    console.log(`Task 11 - Books by ${author}:`, response.data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
-module.exports.general = public_users;
+// Lấy sách theo Tiêu đề (Promise Callback)
+const getBookByTitlePromise = (title) => {
+  axios.get(localURL + "/title/" + title)
+    .then(response => console.log(`Task 11 - Books titled ${title}:`, response.data))
+    .catch(error => console.error("Error:", error));
+};
